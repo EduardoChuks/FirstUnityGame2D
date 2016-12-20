554d865552f111d580344cb36aa7dbab0fc75078
Eduardo Chuquilin <edu240491@gmail.com>
2016-12-20 08:24:47 -0500

    FirstCommit
    Adding initial files of 2D game demo

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float movespeed;
+	public bool moveRight;
+	public bool moveLeft;
+	public bool isJumping;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		moveRight = false;
+		moveLeft = false;
+		isJumping = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			rb.velocity = new Vector2(movespeed, rb.velocity.y);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			rb.velocity = new Vector2 (rb.velocity.x, movespeed * 2);
+		}
+		if (moveLeft) {
+			rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+		}
+		if (moveRight) {
+			rb.velocity = new Vector2(movespeed, rb.velocity.y);
+		}
+		if (isJumping) {
+			rb.velocity = new Vector2 (rb.velocity.x, movespeed * 2);
+		}
+	}
+}
---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour {
+	private Controls player;
+
+	void Start() {
+		player = FindObjectOfType<Controls>();
+	}
+
+	public void LeftArrow() {
+		player.moveRight = false;
+		player.moveLeft = true;
+	}
+
+	public void RightArrow() {
+		player.moveRight = true;
+		player.moveLeft = false;
+	}
+
+	public void ReleaseLeftArrow() {
+		player.moveLeft = false;
+	}
+
+	public void ReleaseRightArrow() {
+		player.moveRight = false;
+
+	}
+
+	public void Jump() {
+		player.isJumping = true;
+	}
+
+	public void Falling() {
+		player.isJumping = false;
+	}
+}
+
